e3848ffa4249e53771db4b9f638cfca525876214
gortron <gordon.lanza@gmail.com>
2019-10-08 13:21:11 +0100

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+  @@all = []
 
+  attr_accessor :title
+  attr_accessor :author
+  attr_accessor :genre
+  attr_accessor :page_count
 
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
